023b3600e3e7efd0e740ed79031df3bd6ef4d397
Chloe <25387744+qimiko@users.noreply.github.com>
2025-10-19 01:21:00 -0700

    oops

--- tools/stats/songsList.php
+++ tools/stats/songsList.php
@@ -29,7 +29,7 @@ if (!empty($_GET['searchName'])) {
 	$searchName = $_GET['searchName'];
 	$query = $db->prepare("SELECT ID,name,download FROM reuploadSongs WHERE name LIKE :query OR authorName LIKE :query OR ID=:base ORDER BY ID DESC LIMIT 500");
 	$query->bindValue(':offs', $page * 500, PDO::PARAM_INT);
-	$query->execute([':query' => "%$searchName%", ':base' => searchName]);
+	$query->execute([':query' => "%$searchName%", ':base' => $searchName]);
 } else {
 	$query = $db->prepare("SELECT ID,name,download FROM reuploadSongs ORDER BY ID DESC LIMIT 500 OFFSET :offs");
 	$query->bindValue(':offs', $page * 500, PDO::PARAM_INT);
