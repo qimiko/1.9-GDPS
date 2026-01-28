c28b47304e3c02f32db8d377ca8659d7e5ab3892
Chloe <25387744+qimiko@users.noreply.github.com>
2026-01-27 20:31:40 -0700

    fix checkupdate url

--- checkUpdate.php
+++ checkUpdate.php
@@ -3,8 +3,9 @@ include './config/latestUpdate.php';
 
 $info = array(
   'version' => $latestVersion['version'],
-  'download_url' => "https://{$_SERVER['HTTP_HOST']}/gdps/download.php"
+  'download_url' => 'https://19gdps.com/download.php'
 );
 
+header('Content-Type: application/json; charset=UTF-8');
 echo json_encode($info, JSON_UNESCAPED_SLASHES);
 ?>
