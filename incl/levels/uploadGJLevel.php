f26fdb83a4e5e66f826468118445b5d522e26398
Chloe <git@celosia.zip>
2026-04-07 13:54:34 -0700

    add additional ratelimit to levels

--- incl/levels/uploadGJLevel.php
+++ incl/levels/uploadGJLevel.php
@@ -129,6 +129,13 @@ $query->execute([':time' => $uploadDate - 60, ':userID' => $userID, ':ip' => $ho
 if($query->fetchColumn() > 0){
 	exit("-1");
 }
+
+$query = $db->prepare("SELECT count(*) FROM levels WHERE uploadDate > :time AND (userID = :userID OR hostname = :ip)");
+$query->execute([':time' => $uploadDate - 3600, ':userID' => $userID, ':ip' => $hostname]);
+if($query->fetchColumn() >= 5){
+        exit("-1");
+}
+
 $query = $db->prepare("INSERT INTO levels (levelName, gameVersion, binaryVersion, userName, levelDesc, levelVersion, levelLength, audioTrack, auto, password, original, twoPlayer, songID, objects, coins, requestedStars, extraString, levelString, levelInfo, secret, uploadDate, userID, extID, updateDate, unlisted, hostname, isLDM, wt, wt2, unlisted2, settingsString, songIDs, sfxIDs, ts)
 VALUES (:levelName, :gameVersion, :binaryVersion, :userName, :levelDesc, :levelVersion, :levelLength, :audioTrack, :auto, :password, :original, :twoPlayer, :songID, :objects, :coins, :requestedStars, :extraString, :levelString, :levelInfo, :secret, :uploadDate, :userID, :id, :uploadDate, :unlisted, :hostname, :ldm, :wt, :wt2, :unlisted2, :settingsString, :songIDs, :sfxIDs, :ts)");
 
